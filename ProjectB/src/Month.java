b4daf0fe8a242668aae5b8d7f3e4f2c4a1db2b66
sunsunnp <sunsun.np@gmail.com>
2018-10-18 15:35:00 +0700

    add month in pjb

--- ProjectB/src/Month.java
+++ ProjectB/src/Month.java
@@ -1,4 +1,27 @@
 
 public class Month {
-
+	
+	String[] month = new String[]{"January", "February", "March", "April", "May" , "June", "July" , "August", "September", "October", "November", "December"};
+	int number;
+	
+	public Month(int number) {
+		this.number = number;
+	}
+	public String[] getMonth() {
+		return month;
+	}
+	public void setMonth(String[] month) {
+		this.month = month;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	public String getMonthbynumber() {
+		return this.month[number];
+	}
+	
 }
