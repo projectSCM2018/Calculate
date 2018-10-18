b7c016ce09426ed8cb94967a118a422310cfa702
godzilowen <owen1491@gmail.com>
2018-10-18 15:43:30 +0700

    fix Main in PJB

--- ProjectB/src/Main.java
+++ ProjectB/src/Main.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Inpout :");
+		System.out.println("Input  :");
 		
 		Scanner in = new Scanner(System.in);
-		int num = in.nextInt()
+		int num = in.nextInt();
 		Month month = new Month(num);
 		
-		System.out.println("OutPut :" + Month.getMonth());
+		System.out.println("Output : " + month.getMonthbynumber());
 		
 	}
 
